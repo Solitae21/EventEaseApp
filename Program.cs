62bc97b041bddcd83e515385be4478f6f420e6ea
Hans Amponin <hans.amponin@asialinkfinance.com.ph>
2025-05-16 11:38:38 +0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+using EventEaseApp;
+using EventEaseApp.Services; // Ensure this matches the namespace of UserSession
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using System.Net.Http;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+// Registering UserSession as a Singleton
+builder.Services.AddSingleton<UserSession>();
+
+// Registering HttpClient (Scoped to the application)
+builder.Services.AddScoped(sp => new HttpClient
+{
+    BaseAddress = new Uri(builder.HostEnvironment.BaseAddress)
+});
+
+await builder.Build().RunAsync();
---
+++ Services/UserSession.cs
@@ -0,0 +1,9 @@
+﻿namespace EventEaseApp.Services
+{
+    public class UserSession
+    {
+        public string UserName { get; set; } = string.Empty;
+        public bool IsRegistered { get; set; } = false;
+        public HashSet<string> RegisteredEvents { get; set; } = new();
+    }
+}
